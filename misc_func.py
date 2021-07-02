3b25500ecb5a001bec2c803d978c88cdef1e8a19
Su-nlight <84061534+Su-nlight@users.noreply.github.com>
2021-07-02 09:00:11 +0530

    Create misc_func.py

---
+++ misc_func.py
@@ -0,0 +1,7 @@
+import random
+
+def intro():
+    res=["Hello! \n I'm Raw Bot. you can call me Rawwy. 😁",
+         "Hey there! \n I would like to introduce myself as Rawwy. \n Officially known as Raw Bot."]
+    res1=random.choice(res)
+    return (res1)
