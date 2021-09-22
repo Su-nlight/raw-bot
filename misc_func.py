d745ac39993a5c2f212792cd21d030c65f41aa7a
Su-nlight <84061534+Su-nlight@users.noreply.github.com>
2021-09-22 19:26:30 +0530

    Update misc_func.py

--- misc_func.py
+++ misc_func.py
@@ -7,19 +7,26 @@ def intro():
     res=["Hello! \n I'm Raw Bot. you can call me Rawwy. 😁",
          "Hey there! \n I would like to introduce myself as Rawwy. \n Officially known as Raw Bot."]
     res1=random.choice(res)
-    return (res1)
+    return res1
 
 def help():
     embed = discord.Embed(title="All Commands",
                           description="$intro - Introduces himself.\n\n"
                                       "$quote - Sends a Random quote.\nCan also be used as '$q' \n \n"
                                       "$sn [Number] - Sends Region and carrier of provided number (with country code)\n\n"
-                                      "$dailyquote - Sends quote of the day.\n Can also be used as '$dq\n\n",
+                                      "$dailyquote - Sends quote of the day.\n Can also be used as '$dq\n\n"
+                                      "$botnews - Sends recent news of bot released by developer \n Can also be used as $bn",
                           color=0xFF5733)
-    return (embed)
+    return embed
 
 def num_search(pn):
     ph = phonenumbers.parse(pn, "CH")
     trialx = geocoder.description_for_number(ph, "en")
     trialy = carrier.name_for_number(ph, "en")
     return(trialx, trialy)
+
+def bot_news():
+    embed = discord.Embed(title="📰📰 New Updates In Raw Bot 📰📰",
+                          description="1) Now You All Can Use Bot Commands Irrespective Of Case.\n       Dated --- 21/09/2021",
+                          color=0x065535)
+    return embed
